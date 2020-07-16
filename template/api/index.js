ac11d9d020844f253bb49d8e1320c22ab93c43d6
SoftProDev <softprodev15@gmail.com>
2020-07-16 21:41:40 +0430

    refactor template (#176)

--- template/api/index.js
+++ template/api/index.js
@@ -1,16 +1,23 @@
 const express = require('express')
 
-// Create express instnace
+// Create express instance
 const app = express()
 
 // Require API routes
 const users = require('./routes/users')
+const test = require('./routes/test')
 
 // Import API Routes
 app.use(users)
+app.use(test)
 
-// Export the server middleware
-module.exports = {
-  path: '/api',
-  handler: app
+// Export express app
+module.exports = app
+
+// Start standalone server if directly running
+if (require.main === module) {
+  const port = process.env.PORT || 3001
+  app.listen(port, () => {
+    console.log(`API server listening on port ${port}`)
+  })
 }
